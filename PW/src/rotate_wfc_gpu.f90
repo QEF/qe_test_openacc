d5595793f8e21c1d1d79530b3304a85403883eac
Sergio Orlandini <s.orlandini@cineca.it>
2021-06-14 15:44:17 +0200

    - Parallel GPU versione of iterative solution of the eigenvalue problem pcegterg_gpu in complex - Parallel GPU versions of rotate_wfc for Gamma-only calculations and colinear, k-point calculations

--- PW/src/rotate_wfc_gpu.f90
+++ PW/src/rotate_wfc_gpu.f90
@@ -43,7 +43,7 @@ SUBROUTINE rotate_wfc_gpu &
 #endif
   COMPLEX(DP), ALLOCATABLE :: psi_h(:,:), evc_h(:,:)
   REAL(DP), ALLOCATABLE    :: e_h(:)
-  EXTERNAL h_psi, s_psi, h_psi_gpu, s_psi_gpu
+  EXTERNAL h_psi_gpu, s_psi_gpu
   !
     ! h_psi(npwx,npw,nvec,psi,hpsi)
     !     calculates H|psi>
@@ -51,49 +51,34 @@ SUBROUTINE rotate_wfc_gpu &
     !     calculates S|psi> (if needed)
     !     Vectors psi,hpsi,spsi are dimensioned (npwx,npol,nvec)
   !
-  CALL start_clock_gpu( 'wfcrot' ); !write (*,*) 'start wfcrot' ; FLUSH(6)
-  !write (*,*) 'gamma_only' , gamma_only; FLUSH(6)
+  CALL start_clock_gpu( 'wfcrot' )
 
   IF( use_para_diag ) THEN
-     !
-     ! Allocate arrays to workaround parallel case
-     !
-     ALLOCATE (psi_h(npwx*npol,nstart), evc_h(npwx*npol,nbnd), e_h(nbnd))
-     psi_h(1:npwx*npol,1:nstart) = psi_d(1:npwx*npol,1:nstart)
-     evc_h(1:npwx*npol,1:nbnd) = evc_d(1:npwx*npol,1:nbnd)
      !
      ! use data distributed subroutine
      !
      IF ( gamma_only ) THEN
-  !write (*,*) 'inside para gamma'; FLUSH(6)
         !
-        CALL protate_wfc_gamma ( h_psi, s_psi, overlap, &
-                                 npwx, npw, nstart, nbnd, psi_h, evc_h, e_h )
+        CALL protate_wfc_gamma_gpu ( h_psi_gpu, s_psi_gpu, overlap, &
+                                     npwx, npw, nstart, nbnd, psi_d, evc_d, e_d )
         !
      ELSE
-  !write (*,*) 'inside para k'; FLUSH(6)
         !
-        CALL protate_wfc_k ( h_psi, s_psi, overlap, &
-                             npwx, npw, nstart, nbnd, npol, psi_h, evc_h, e_h )
+        CALL protate_wfc_k_gpu ( h_psi_gpu, s_psi_gpu, overlap, &
+                                 npwx, npw, nstart, nbnd, npol, psi_d, evc_d, e_d )
         !
      END IF
-     psi_d(1:npwx*npol,1:nstart) = psi_h(1:npwx*npol,1:nstart)
-     evc_d(1:npwx*npol,1:nbnd)   = evc_h(1:npwx*npol,1:nbnd)
-     e_d(1:nbnd)                 = e_h(1:nbnd)
-     DEALLOCATE(psi_h, evc_h, e_h)
      !
   ELSE
      !
      ! use serial subroutines
      !
      IF ( gamma_only ) THEN
-  !write (*,*) 'inside serial gamma'; FLUSH(6)
         !
         CALL rotate_wfc_gamma_gpu ( h_psi_gpu, s_psi_gpu, overlap, &
                                     npwx, npw, nstart, nbnd, psi_d, evc_d, e_d )
         !
      ELSE
-  !write (*,*) 'inside serial k'; FLUSH(6)
         !
         CALL rotate_wfc_k_gpu ( h_psi_gpu, s_psi_gpu, overlap, &
                                 npwx, npw, nstart, nbnd, npol, psi_d, evc_d, e_d )
@@ -102,6 +87,6 @@ SUBROUTINE rotate_wfc_gpu &
      !
   END IF
   !
-  CALL stop_clock_gpu( 'wfcrot' )!; write (*,*) 'stop wfcrot' ; FLUSH(6)
+  CALL stop_clock_gpu( 'wfcrot' )
   !
 END SUBROUTINE rotate_wfc_gpu

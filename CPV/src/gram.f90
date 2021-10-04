be16417ab29d00144bd8f54aa1c823730befb5b1
Pietro Delugas <no-reply@pietrodelugas.it>
2021-10-04 09:50:32 +0200

    save temp status of mygram porting

--- CPV/src/gram.f90
+++ CPV/src/gram.f90
@@ -49,6 +49,7 @@ SUBROUTINE gram_bgrp( betae, bec_bgrp, nkbx, cp_bgrp, ngwx )
       ALLOCATE( bec_tmp( nkbx ) )
       ALLOCATE( csc2( SIZE( csc ) ) )
 !
+      !$acc data copy(cp_bgrp, bec_bgrp) create(csc, ctmp, cp_tmp,, bec_tmp, csc2) 
       DO iss = 1, nspin
       DO i = iupdwn(iss), iupdwn(iss) + nupdwn(iss) - 1 
          !
@@ -59,21 +60,29 @@ SUBROUTINE gram_bgrp( betae, bec_bgrp, nkbx, cp_bgrp, ngwx )
          ! calculate orthogonalized cp(i) : |cp(i)>=|cp(i)>-\sum_k<i csc(k)|cp(k)>
          !
          IF( ibgrp_i > 0 ) THEN
+            !$acc kernels present (cp_bgrp, ctmp) 
             ctmp = cp_bgrp( :, ibgrp_i )
+            !$acc end kernels
          ELSE
+            !$acc kernels present (ctmp)
             ctmp = 0.0d0
+            !$acc end kernels
          END IF
          !
-         IF( nbgrp_im1 > 0 .AND. ngw > 0 ) &
-            CALL dgemv( 'N', 2*ngw, nbgrp_im1, mone, cp_bgrp(1,iupdwn_bgrp(iss)), 2*ngwx, csc, 1, one, ctmp, 1 )
-
+         IF( nbgrp_im1 > 0 .AND. ngw > 0 ) THEN 
+            CALL my_dgemv( 'N', 2*ngw, nbgrp_im1, mone, cp_bgrp(1,iupdwn_bgrp(iss)), 2*ngwx, csc, 1, one, ctmp, 1 )
+         END IF 
          CALL mp_sum( ctmp, inter_bgrp_comm )
 
          IF( ibgrp_i > 0 ) THEN
+            !$acc kernels present(ctmp)
             cp_bgrp( :, ibgrp_i ) = ctmp
+            !$acc end kernels
             anorm = cscnorm( bec_bgrp, cp_bgrp, ibgrp_i, nbspx_bgrp )
+            !$acc kernels
             cp_bgrp(:,ibgrp_i) = cp_bgrp(:,ibgrp_i) / anorm
             bec_bgrp(:,ibgrp_i) = bec_bgrp(:,ibgrp_i) / anorm
+            !$acc end kernels 
          END IF
       END DO
       END DO
@@ -114,26 +123,44 @@ CONTAINS
       REAL(DP) :: cscnorm, ddot
       !
       INTEGER  :: is, iv, jv, ia, indv
-      REAL(DP) rsum
+      REAL(DP) rsum, rsum_aug
+      LOGICAL,ALLOCATABLE :: tvamp(:)
 !
+#if defined (_OPENACC) 
+      !$acc kernels present(cp) 
+      rsum = 2.d0 * dot_product(cp(1:ngw,i),cp(1:ngw,i))
+      rsum = rsum - g0 * REAL(CONJG(cp(1,i))*cp(1,i),DP) 
+      !$acc end kernels
+#else 
       rsum = 2.d0 * ddot(2*ngw,cp(1,i),1,cp(1,i),1) - g0 * REAL(CONJG(cp(1,i))*cp(1,i), DP)
+#endif 
 !
+      ALLOCATE (tvamp(SIZE(upf))) 
+      DO is = 1, SIZE (upf)
+         tvamp(is) = upf(i)%tvanp
+      END DO 
+      rsum_aug = 0._DP
+      !$acc parallel private(ia, is, indv, iv, jv) reduction(+:rsum_aug) vector_length(32)
+      !$acc& present(bec) copyin(qq_nt,nh, tvamp, ofsbeta) 
+      !$acc loop gang
       DO ia=1,nat
          IF ( MOD( ia, nproc_bgrp ) == me_bgrp ) THEN
             is = ityp(ia)
-            IF( upf(is)%tvanp ) THEN
+            IF( tvanp(is) ) THEN
                indv = ofsbeta(ia)
+               !$acc loop vector
                DO iv=1,nh(is)
                   DO jv=1,nh(is)
                      IF(ABS(qq_nt(iv,jv,is)).GT.1.e-5) THEN
-                        rsum = rsum + qq_nt(iv,jv,is)*bec(indv+iv,i)*bec(indv+jv,i)
+                        rsum_aug = rsum_aug + qq_nt(iv,jv,is)*bec(indv+iv,i)*bec(indv+jv,i)
                      ENDIF
                   END DO
                END DO
             END IF
          END IF
       END DO
-
+      !$acc end parallel 
+      rsum = rsum + rsum_aug 
       CALL mp_sum( rsum, intra_bgrp_comm )
 !
       cscnorm=SQRT(rsum)
@@ -170,13 +197,19 @@ CONTAINS
       !
       kmax = i - 1
       !
+      !$acc kernels present(csc) 
       csc    = 0.0d0
+      !$acc end kernels
 
       ibgrp_i = ibgrp_g2l( i )
       IF( ibgrp_i > 0 ) THEN
+         !$acc kernels present(cp_tmp,cp_bgrp) 
          cp_tmp = cp_bgrp( :, ibgrp_i )
+         !$acc end kernels
       ELSE
+         !$acc kernels present(cp_tmp)
          cp_tmp = 0.0d0
+         !$acc end kernels
       END IF
 
       CALL mp_sum( cp_tmp, inter_bgrp_comm )
@@ -191,10 +224,14 @@ CONTAINS
       END DO
       kmax_bgrp = kmax_bgrp - iupdwn_bgrp(iss) + 1
 
-      IF( kmax_bgrp > 0 .AND. ngw > 0 ) &
-        CALL dgemv( 'T', 2*ngw, kmax_bgrp, 1.0d0, cp_bgrp(1,iupdwn_bgrp(iss)), 2*ngwx, cp_tmp, 1, 0.0d0, csc2, 1 )
+      IF( kmax_bgrp > 0 .AND. ngw > 0 ) THEN 
+        !$acc data present(cp_bgrp, cp_tmp, csc2 ) 
+        CALL my_dgemv( 'T', 2*ngw, kmax_bgrp, 1.0d0, cp_bgrp(1,iupdwn_bgrp(iss)), 2*ngwx, cp_tmp, 1, 0.0d0, csc2, 1 )
+        !$acc end data
+      END IF 
 
       nk = 0
+      !$acc kernels present(csc, csc2, cp_bgrp, cp_tmp) 
       DO k = iupdwn( iss ), kmax
          ibgrp_k = ibgrp_g2l( k )
          IF( ibgrp_k > 0 ) THEN
@@ -202,6 +239,8 @@ CONTAINS
             csc(k) = 2.0d0 * csc2(nk) - g0 * DBLE( cp_bgrp(1,ibgrp_k) * CONJG(cp_tmp(1)) )
          END IF
       END DO
+      !$acc end kernels
+
 
       IF(  ibgrp_i > 0 ) THEN
          DO ia = 1, nat
@@ -274,7 +313,9 @@ CONTAINS
       END DO
 
       IF( nk > 0 .AND. ngw > 0 ) THEN
-        CALL dgemv( 'N', nkbx, nk, -1.0d0, bec_bgrp(1,iupdwn_bgrp(iss)), nkbx, csc, 1, 0.0d0, bec_tmp, 1 )
+        !$acc data copyin(bec_bgrp, csc) copyout(bec_tmp) 
+        CALL my_dgemv( 'N', nkbx, nk, -1.0d0, bec_bgrp(1,iupdwn_bgrp(iss)), nkbx, csc, 1, 0.0d0, bec_tmp, 1 )
+        !$acc end data
       ELSE
         bec_tmp = 0.0d0
       END IF
@@ -286,3 +327,33 @@ CONTAINS
       END SUBROUTINE gracsc_bgrp
 
 END SUBROUTINE gram_bgrp
+
+SUBROUTINE my_dgemv(t, m, n, alpha, a, lda, x, incx, beta, y, incy) 
+USE kinds, ONLY: DP
+#if defined(_OPENACC) 
+  USE cublas 
+#endif 
+  IMPLICIT NONE 
+  CHARACTER       :: t 
+  INTEGER         :: m,n,lda,incx, incy 
+  REAL(DP)        :: alpha, beta 
+  REAL(DP)        :: a (lda, *), x(*), y(*)
+  ! 
+ INTEGER         :: dim_x, dim_y
+ SELECT CASE (t) 
+   CASE ('n','N') 
+     dim_x = 1 + (n-1) * abs(incx) 
+     dim_y = 1 + (m-1) * abs(incy) 
+   CASE default 
+    dim_x = 1 + (m-1) * abs(incx) 
+    dim_y = 1 + (n-1) * abs(incy)    
+ END SELECT 
+ !!$acc data copyin(a(1:m,1:n),x(dim_x)) copy(y(dim_y)) 
+ !$acc host_data use_device(a,x,y) if_present
+   CALL cublasdgemv(t, m, n, alpha, a, lda, x, incx, beta, y, incy) 
+ !$acc end host_data
+ !!$acc end data 
+END SUBROUTINE my_dgemv 
+
+
+

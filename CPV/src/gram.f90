dfe142b03847338c1a60280f62393170018df632
Pietro Delugas <no-reply@pietrodelugas.it>
2021-10-08 08:44:16 +0200

    added openACC  in gram.f90

--- CPV/src/gram.f90
+++ CPV/src/gram.f90
@@ -21,8 +21,6 @@
  #define START_WSHARE DEV_OMP workshare
  #define END_WSHARE   DEV_OMP end workshare
 #endif
-#define DEV_ACC !!!
-#define _DEV_ACC !$acc
 !-------------------------------------------------------------------------
 SUBROUTINE gram_bgrp( betae, bec_bgrp, nkbx, cp_bgrp, ngwx )
 !-----------------------------------------------------------------------
@@ -75,7 +73,7 @@ SUBROUTINE gram_bgrp( betae, bec_bgrp, nkbx, cp_bgrp, ngwx )
       CALL set_uspp_stuff(tvanp, all_tvanp, nqq, iqq, nsp, upf, nh, qq_nt)
       CALL block_distribute(nat, me_bgrp, nproc_bgrp, ia_s, ia_e, mykey)  
 DEV_ACC data copy(cp_bgrp, bec_bgrp) create(ctmp, cp_tmp,bec_tmp,csc,csc2) &
-DEV_ACC & copyin(qq_nt, ofsbeta,ityp, ibgrp_g2l, nh, tvanp ) 
+DEV_ACC & copyin(betae, qq_nt, ofsbeta,ityp, ibgrp_g2l, nh, tvanp ) 
       DO iss = 1, nspin
       DO i = iupdwn(iss), iupdwn(iss) + nupdwn(iss) - 1 
          !
@@ -97,20 +95,16 @@ DEV_ACC end kernels
          !
          IF( nbgrp_im1 > 0 .AND. ngw > 0 ) THEN 
 #if defined (__OPENACC)
-_DEV_ACC data copy(cp_bgrp, csc, ctmp)  
-_DEV_ACC host_data use_device(cp_bgrp, csc, ctmp) 
+DEV_ACC host_data use_device(cp_bgrp, csc, ctmp) 
            CALL mydgemv( 'N', 2*ngw, nbgrp_im1, mone, cp_bgrp(1,iupdwn_bgrp(iss)), 2*ngwx, csc, 1, one, ctmp, 1 )
-_DEV_ACC end host_data
-_DEV_ACC end data
+DEV_ACC end host_data
 #else
            CALL dgemv( 'N', 2*ngw, nbgrp_im1, mone, cp_bgrp(1,iupdwn_bgrp(iss)), 2*ngwx, csc, 1, one, ctmp, 1 )
 #endif
          END IF 
-!!DEV_ACC host_data use_device(ctmp) 
-DEV_ACC update self(ctmp) 
+DEV_ACC host_data  use_device(ctmp) 
          CALL mp_sum( ctmp, inter_bgrp_comm )
-DEV_ACC update device(ctmp) 
-!!!DEV_ACC end host_data 
+DEV_ACC end host_data
          IF( ibgrp_i > 0 ) THEN
 DEV_ACC kernels present(cp_bgrp, ctmp) 
             cp_bgrp( :, ibgrp_i ) = ctmp
@@ -192,43 +186,44 @@ CONTAINS
       REAL(DP) :: cscnorm, ddot
       !
       INTEGER  :: is, iv, jv, ia, indv
-      REAL(DP) rsum
+      REAL(DP) rsum, rsum_v
       REAL(DP), EXTERNAL  :: myddot 
 !
-_DEV_ACC data copyin (bec, cp, tvanp,ofsbeta, nh)  
+DEV_ACC data present(bec, cp, tvanp,ofsbeta, nh, ityp, qq_nt)  
 #if defined(__OPENACC) 
-DEV_ACC data copyin (cp)   
-_DEV_ACC host_data use_device(cp) 
+DEV_ACC host_data use_device(cp) 
       rsum = 2.d0 * myddot(2*ngw,cp(1,i),1,cp(1,i),1) 
-_DEV_ACC end host_data
-DEV_ACC end data
+DEV_ACC end host_data
 #else
        rsum = 2.d0 * ddot(2*ngw,cp(1,i),1,cp(1,i),1) 
 #endif  
-_DEV_ACC kernels
+DEV_ACC kernels present(cp) 
       rsum = rsum - g0 * REAL(CONJG(cp(1,i))*cp(1,i), DP)
-_DEV_ACC end kernels 
+DEV_ACC end kernels 
 !
-_DEV_ACC parallel private(ia, is, iv, jv) reduction (+:rsum) vector_length(32) 
-_DEV_ACC loop gang  
+DEV_ACC parallel private(ia, is, iv, jv, rsum_v) reduction (+:rsum) vector_length(32) & 
+DEV_ACC & present(ityp, ofsbeta, tvanp,nh,qq_nt,bec)  
+DEV_ACC loop gang  
       DO ia= ia_s, ia_e 
          IF ( mykey == 0 ) THEN
             is = ityp(ia)
             IF( tvanp( is )) THEN
+               rsum_v = 0._DP
                indv = ofsbeta(ia)
-_DEV_ACC loop vector collapse(2) 
+DEV_ACC loop vector collapse(2) reduction(+:rsum_v)
                DO iv=1,nh(is)
                   DO jv=1,nh(is)
                      IF(ABS(qq_nt(iv,jv,is)).GT.1.e-5) THEN
-                        rsum = rsum + qq_nt(iv,jv,is)*bec(indv+iv,i)*bec(indv+jv,i)
+                        rsum_v = rsum_v + qq_nt(iv,jv,is)*bec(indv+iv,i)*bec(indv+jv,i)
                      ENDIF
                   END DO
                END DO
+               rsum = rsum + rsum_v 
             END IF
          END IF
       END DO
-_DEV_ACC end parallel 
-_DEV_ACC end data
+DEV_ACC end parallel 
+DEV_ACC end data
       CALL mp_sum( rsum, intra_bgrp_comm )
 !
       cscnorm=SQRT(rsum)
@@ -266,23 +261,26 @@ _DEV_ACC end data
       !
       kmax = i - 1
       !
-_DEV_ACC data copy(csc,csc2, cp_tmp, cp_bgrp, bec_tmp, bec_bgrp) copyin(ibgrp_g2l, nh, ofsbeta, qq_nt, ityp, tvanp)  
-_DEV_ACC kernels present(csc)  
+DEV_ACC data present(csc,csc2, cp_tmp, cp_bgrp, bec_tmp, bec_bgrp) &
+DEV_ACC & present(ibgrp_g2l, nh, ofsbeta, qq_nt, ityp, tvanp, betae)  
+DEV_ACC kernels present(csc)  
       csc    = 0.0d0
-_DEV_ACC end kernels 
+DEV_ACC end kernels 
       ibgrp_i = ibgrp_g2l( i )
-_DEV_ACC kernels present(cp_tmp) 
+DEV_ACC kernels present(cp_tmp) 
       IF( ibgrp_i > 0 ) THEN
          cp_tmp = cp_bgrp( :, ibgrp_i )
       ELSE
          cp_tmp = 0.0d0
       END IF
-_DEV_ACC end kernels 
+DEV_ACC end kernels 
 !!DEV_ACC host_data use_device(cp_tmp) 
 
-_DEV_ACC update self(cp_tmp) 
+!!DEV_ACC update self(cp_tmp)
+DEV_ACC host_data use_device(cp_tmp)  
       CALL mp_sum( cp_tmp, inter_bgrp_comm )
-_DEV_ACC update device(cp_tmp) 
+!!DEV_ACC update device(cp_tmp) 
+DEV_ACC end host_data
 !!DEV_ACC end host_data
       kmax_bgrp = 0
       nk = 0
@@ -296,18 +294,16 @@ _DEV_ACC update device(cp_tmp)
 
       IF( kmax_bgrp > 0 .AND. ngw > 0 ) THEN
 #if defined(__OPENACC)
-DEV_ACC data copyin(cp_bgrp, cp_tmp) copyout(csc2) 
-_DEV_ACC host_data use_device(cp_bgrp, cp_tmp, csc2)  
+DEV_ACC host_data use_device(cp_bgrp, cp_tmp, csc2)  
         CALL mydgemv( 'T', 2*ngw, kmax_bgrp, 1.0d0, cp_bgrp(1,iupdwn_bgrp(iss)), 2*ngwx, cp_tmp, 1, 0.0d0, csc2, 1 )
-_DEV_ACC end host_data
-DEV_ACC end data 
+DEV_ACC end host_data
 #else
         CALL dgemv( 'T', 2*ngw, kmax_bgrp, 1.0d0, cp_bgrp(1,iupdwn_bgrp(iss)), 2*ngwx, cp_tmp, 1, 0.0d0, csc2, 1 )
 #endif 
       END IF 
       nk = 0
       iupdwn_iss = iupdwn( iss) 
-_DEV_ACC serial  present(ibgrp_g2l)      
+DEV_ACC serial  present(ibgrp_g2l)      
       DO k = iupdwn_iss, kmax
          ibgrp_k = ibgrp_g2l( k )
          IF( ibgrp_k > 0 ) THEN
@@ -315,11 +311,11 @@ _DEV_ACC serial  present(ibgrp_g2l)
             csc(k) = 2.0d0 * csc2(nk) - g0 * DBLE( cp_bgrp(1,ibgrp_k) * CONJG(cp_tmp(1)) )
          END IF
       END DO
-_DEV_ACC end serial  
+DEV_ACC end serial  
 
       IF(  ibgrp_i > 0 ) THEN
-_DEV_ACC parallel private(bec_tmp_inl, inl, is, ia, iv) vector_length(32) 
-_DEV_ACC loop gang 
+DEV_ACC parallel private(bec_tmp_inl, inl, is, ia, iv) vector_length(32) 
+DEV_ACC loop gang 
          DO ia = 1, nat
             is = ityp(ia)
             DO iv=1,nh(is)
@@ -329,7 +325,7 @@ _DEV_ACC loop gang
                               - g0 * DBLE(cp_bgrp(1,ibgrp_i) * CONJG(betae(1,inl)))
 #else 
                bec_tmp_inl = 0._DP
-_DEV_ACC loop vector reduction(+:bec_tmp_inl)  
+DEV_ACC loop vector reduction(+:bec_tmp_inl)  
                DO ig =1, ngw
                   bec_tmp_inl = bec_tmp_inl + DBLE(CONJG(cp_bgrp(ig,ibgrp_i))*betae(ig,inl)) 
                END DO 
@@ -339,26 +335,26 @@ _DEV_ACC loop vector reduction(+:bec_tmp_inl)
                bec_tmp(inl) = bec_tmp_inl 
             END DO
          END DO
-_DEV_ACC end parallel 
+DEV_ACC end parallel 
 !!!DEV_ACC host_data use_device(bec_tmp)
-DEV_ACC update self(bec_tmp) 
-_DEV_ACC host_data use_device (bec_tmp)  
+DEV_ACC host_data use_device (bec_tmp)  
          CALL mp_sum( bec_tmp, intra_bgrp_comm )  ! parallel sum over G vectors within a band group
-DEV_ACC update device(bec_tmp) 
-_DEV_ACC end host_data
+DEV_ACC end host_data
 !!!DEV_ACC end host_data
-_DEV_ACC kernels 
+DEV_ACC kernels 
          bec_bgrp( : , ibgrp_i ) = bec_tmp( : )
-_DEV_ACC end kernels 
+DEV_ACC end kernels 
       ELSE
-_DEV_ACC kernels 
+DEV_ACC kernels 
          bec_tmp = 0.0d0
-_DEV_ACC end kernels 
+DEV_ACC end kernels 
       END IF
 !!!DEV_ACC host_data use_device(bec_tmp) 
-_DEV_ACC update self(bec_tmp) 
+!!DEV_ACC update self(bec_tmp) 
+DEV_ACC host_data use_device (bec_tmp) 
       CALL mp_sum( bec_tmp, inter_bgrp_comm )
-_DEV_ACC update device(bec_tmp) 
+DEV_ACC end host_data
+!!DEV_ACC update device(bec_tmp) 
 !!!DEV_ACC end host_data
 !
 !     calculate csc(k)=<cp(i)|S|cp(k)>,  k<i
@@ -366,9 +362,7 @@ _DEV_ACC update device(bec_tmp)
 DEV_ACC kernels 
       csc2    = 0.0d0
 DEV_ACC end kernels 
-
 iupdwn_iss = iupdwn(iss) 
-
 DEV_OMP parallel if( (kmax - iupdwn_iss ) > omp_get_num_threads() ) default(none), &
 DEV_OMP shared(iupdwn_iss,iss,kmax,nproc_bgrp,me_bgrp,nbspx,i,ibgrp_g2l,nh), &
 DEV_OMP shared(ofsbeta,qq_nt,na,bec_tmp,bec_bgrp,csc2,nat,ityp,tvanp, ia_s, ia_e, mykey), &
@@ -415,15 +409,13 @@ DEV_ACC end parallel
 !
 !     corresponing bec:  bec(i)=<cp(i)|beta>-csc(k)<cp(k)|beta>
 !
-!!DEV_ACC host_data use_device(csc, csc2) 
-DEV_ACC update self(csc, csc2) 
+DEV_ACC host_data use_device(csc, csc2) 
       CALL mp_sum( csc, intra_bgrp_comm )
       CALL mp_sum( csc2, intra_bgrp_comm )
       CALL mp_sum( csc, inter_bgrp_comm )
       CALL mp_sum( csc2, inter_bgrp_comm )
-DEV_ACC update device(csc, csc2) 
-!!DEV_ACC end host_data 
-DEV_ACC kernels 
+DEV_ACC end host_data 
+DEV_ACC kernels present(csc, csc2)  
       csc = csc + csc2
 DEV_ACC end kernels 
 
@@ -441,25 +433,23 @@ DEV_ACC end serial
 
       IF( nk > 0 .AND. ngw > 0 ) THEN
 #if defined (__OPENACC)
-_DEV_ACC data copyin(bec_bgrp, csc) copyout(bec_tmp)  
-_DEV_ACC host_data use_device(bec_bgrp, csc, bec_tmp) 
+DEV_ACC data copyin(bec_bgrp, csc) copyout(bec_tmp) 
+DEV_ACC host_data use_device(bec_bgrp, csc, bec_tmp) 
         CALL mydgemv( 'N', nkbx, nk, -1.0d0, bec_bgrp(1,iupdwn_bgrp(iss)), nkbx, csc, 1, 0.0d0, bec_tmp, 1 )
-_DEV_ACC end host_data
-_DEV_ACC end data  
+DEV_ACC end host_data
+DEV_ACC end data 
 #else
         CALL dgemv( 'N', nkbx, nk, -1.0d0, bec_bgrp(1,iupdwn_bgrp(iss)), nkbx, csc, 1, 0.0d0, bec_tmp, 1 )
 #endif
       ELSE
-DEV_ACC kernels 
+DEV_ACC kernels present(bec_tmp) 
         bec_tmp = 0.0d0
 DEV_ACC end kernels 
       END IF
-!!!DEV_ACC host_data use_device(bec_tmp) 
-DEV_ACC update self(bec_tmp) 
+DEV_ACC host_data use_device(bec_tmp)  
       CALL mp_sum( bec_tmp, inter_bgrp_comm )
-DEV_ACC update device(bec_tmp) 
-!!!DEV_ACC end host_data 
-DEV_ACC kernels 
+DEV_ACC end host_data 
+DEV_ACC kernels  present(bec_bgrp, bec_tmp) 
       IF( ibgrp_i > 0 ) bec_bgrp(:,ibgrp_i ) = bec_bgrp(:,ibgrp_i ) + bec_tmp
 DEV_ACC end kernels 
 DEV_ACC end data 
